f74652807f2ded8937374b37179da7ffcc15d7b1
CruzGusta <165952673+CruzGusta@users.noreply.github.com>
2025-05-28 02:38:01 +0000

    codigo re-estruturdo para melhor visualização e adicionado novos recursos do nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,86 +10,106 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     // variaveis
-    char estado;
-    char codigo [5];
-    char nomeCid [30];
-    int populacao, Pturisticos;
-    float areaKm, pib, densiPop, PIBc;
+    char estadoA, estadoB;
+    char codigoA [5], codigoB [5];
+    char nomeCidA [30], nomeCidB [30];
+    int PturisticosA, PturisticosB, resultado;
+    float areaKmA, areaKmB, pibA, pibB, densiPopA, densiPopB, PIBca, PIBcb, densiPopREa, densiPopREb;
+    unsigned long int populacaoA, populacaoB; // uso de unsigned para grandes numeros
 
-    // perguntas para cadastro
+    // perguntas para cadastro carta 1
+    printf("--cadastro da carta A-- \n");
     printf(" Estado : \n");
-    scanf("%c", &estado );
+    scanf(" %c", &estadoA );
 
     printf("Código da carta : \n");
-    scanf(" %s", codigo);
+    scanf(" %s", codigoA);
 
     printf("Nome da cidade : \n");
-    scanf(" %s", nomeCid);
+    scanf(" %s", nomeCidA);
 
     printf("População : \n");
-    scanf("%d", &populacao);
+    scanf("%lu", &populacaoA);
 
-    printf("Área em km : \n");
-    scanf("%f",&areaKm );
+    printf("Área em km² : \n");
+    scanf("%f",&areaKmA );
 
     printf("PIB : \n");
-    scanf("%f",&pib );
+    scanf("%f",&pibA );
 
     printf("Pontos turisticos : \n");
-    scanf("%d", &Pturisticos);
+    scanf("%d", &PturisticosA);
 
-    densiPop = populacao/areaKm; // calculo da densidade ppulacional
-    PIBc = (float) pib/populacao; // calculo do PIB per capita com casting
-    
+    densiPopA = populacaoA/areaKmA; // calculo da densidade ppulacional
+    PIBca = (float) pibA/populacaoA; // calculo do PIB per capita com casting
+    densiPopREa = (float) areaKmA/populacaoA; // densidade populaconal reverso
+    float SuperPoderA = (float) populacaoA + areaKmA + pibA + PturisticosA + PIBca + densiPopREa;
 
-
-// print dos resultados
-
-   printf("Estado : %c \n Código da carta : %s \n", estado, codigo);
-   printf("Nome da cidade : %s \n População : %d \n",nomeCid, populacao );
-   printf("Àrea : %.2f km \n PIB : %.2f \n", areaKm, pib);
-   printf("Pontos turisticos : %d \n", Pturisticos);
-   printf("Densidade populacional : %.2f \n", densiPop);
-   printf("PIB per capita : %.2f \n", PIBc);
-   getchar();
-
-
+    getchar();
 //carta 2
        // perguntas para cadastro
+    printf("--cadastro da carta B-- \n");
     printf("Estado : \n"); 
-    scanf("%c", &estado );
+    scanf(" %c", &estadoB );
 
     printf("Código da carta : \n");
-    scanf(" %s", &codigo);
+    scanf(" %s", &codigoB);
 
     printf("Nome da cidade : \n");
-    scanf(" %s", &nomeCid);
+    scanf(" %s", &nomeCidB);
 
     printf("População : \n");
-    scanf("%d", &populacao);
+    scanf("%lu", &populacaoB);
 
-    printf("Área em km : \n");
-    scanf("%f",&areaKm );
+    printf("Área em km² : \n");
+    scanf("%f",&areaKmB );
 
     printf("PIB : \n");
-    scanf("%f",&pib );
+    scanf("%f",&pibB );
 
 
     printf("Pontos turisticos : \n");
-    scanf("%d", &Pturisticos);
+    scanf("%d", &PturisticosB);
 
-    densiPop = populacao/areaKm;
-    PIBc = (float) pib/populacao;
+    densiPopB = populacaoB/areaKmB;
+    PIBcb = (float) pibB/populacaoB;
+    densiPopREb = (float) areaKmB/populacaoB; // densidade populaconal reverso
+    float SuperPoderB = (float) populacaoB+ areaKmB + pibB + PturisticosB + PIBcb + densiPopB;
+
+    
+// print dos resultados da carta A
+   printf("     \n"); // espaço de linha ra estética
+   printf("**carta A** \n");
+   printf("Estado : %c \n Código da carta : %s \n", estadoA, codigoA);
+   printf("Nome da cidade : %s \n População : %lu \n",nomeCidA, populacaoA );
+   printf("Àrea : %.2f km² \n PIB : %.2f \n", areaKmA, pibA);
+   printf("Pontos turisticos : %d \n", PturisticosA);
+   printf("Densidade populacional : %.2f \n", densiPopA);
+   printf("PIB per capita : %.2f \n", PIBca);
+   printf("Super poder : %.2f \n", SuperPoderA);
+
+// print dos resultados da carta B
+   printf("     \n"); // espaço de linha ra estética
+   printf("**carta B** \n");
+   printf("Estado : %c \n Código da carta : %s \n", estadoB, codigoB);
+   printf("Nome da cidade : %s \n População : %lu \n",nomeCidB, populacaoB );
+   printf("Àrea : %.2f km² \n PIB : %.2f \n", areaKmB, pibB);
+   printf("Pontos turisticos : %d \n", PturisticosB);
+   printf("Densidade populacional : %.2f \n", densiPopB);
+   printf("PIB per capita : %.2f \n", PIBcb);
+   printf("Super poder : %.2f\n", SuperPoderB);
 
 
-// print dos resultados
+// comparações das cartas
+   printf("     \n"); // espaço de linha ra estética
+   printf("População: %d\n", populacaoA > populacaoB);
+   printf("Área: %d\n", areaKmA > areaKmB);
+   printf("PIB: %d\n", pibA > pibB);
+   printf("Pontos turisticos: %d\n", PturisticosA > PturisticosB);
+   printf("Densidade Populacional: %d\n", densiPopA < densiPopB);
+   printf("PIB per Capita: %d\n", PIBca > PIBcb);
+   printf("Super Poder: %d\n", SuperPoderA > SuperPoderB);
 
-   printf("Estado : %c \n Código da carta : %s \n", estado, codigo);
-   printf("Nome da cidade : %s \n População : %d \n",nomeCid, populacao );
-   printf("Àrea : %.2f km \n PIB : %.2f \n", areaKm, pib);
-   printf("Pontos turisticos : %d \n", Pturisticos);
-   printf("Densidade populacional : %.2f \n", densiPop);
-   printf("PIB per capita : %.2f \n", PIBc);
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
